9711c591ad02049f731ae4775ec73b38fe7ec1d6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-17 09:39:37 +0100

    lancio di due dadi, con clacolo della somma

--- TiroDadi/TreDadi/MainWindow.xaml.cs
+++ TiroDadi/TreDadi/MainWindow.xaml.cs
@@ -20,9 +20,24 @@ namespace TreDadi
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random tiro = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btn_tira_Click(object sender, RoutedEventArgs e)
+        {
+            //primo tiro
+            int faccia1= tiro.Next(1, 7); ;
+            img_dado1.Source = new BitmapImage(new Uri($"C:/Users/studenti/source/repos/TiroDadi/TiroDadi/TreDadi/dado_{faccia1}.jpg"));
+
+            //secondo tiro
+            int faccia2 = tiro.Next(1, 7); ;
+            img_dado2.Source = new BitmapImage(new Uri($"C:/Users/studenti/source/repos/TiroDadi/TiroDadi/TreDadi/dado_{faccia2}.jpg"));
+
+            //calcolo somma
+            lbl_somma.Content = faccia1 + faccia2;
         }
     }
 }
